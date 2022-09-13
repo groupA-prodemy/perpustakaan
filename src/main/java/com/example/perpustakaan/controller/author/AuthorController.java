f7b1beaf00f557cc5e3cb0f316f51bf047b340eb
Rosita120505 <107414498+Rosita120505@users.noreply.github.com>
2022-09-13 11:15:51 +0700

    CRUD Author

--- src/main/java/com/example/perpustakaan/controller/author/AuthorController.java
+++ src/main/java/com/example/perpustakaan/controller/author/AuthorController.java
@@ -1,16 +1,17 @@
 package com.example.perpustakaan.controller.author;
 
+import com.example.perpustakaan.model.dto.AdminDto;
 import com.example.perpustakaan.model.dto.DefaultResponse;
+import com.example.perpustakaan.model.entity.Admin;
 import com.example.perpustakaan.model.entity.Author;
 import com.example.perpustakaan.model.dto.AuthorDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.perpustakaan.repository.AuthorRepository;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -30,12 +31,72 @@ public class AuthorController {
             response.setMessage("Error, Data Sudah Tersedia");
         } else {
             authorRepository.save(author);
-//            addressRepository.save(address);
             response.setStatus(Boolean.TRUE);
             response.setMessage("Berhasil Simpan Data");
         }
 
         return response;
+    }
+
+    @GetMapping("/all") //menampilkan all author
+    public List<AuthorDto> getListAuthor() {
+        List<AuthorDto> list = new ArrayList<>();
+        for (Author a : authorRepository.findAll()) {
+            list.add(convertEntitytoDto(a));
+        }
+        return list;
+    }
+
+    @DeleteMapping("/delete/{authorid}")
+    public DefaultResponse deleteByAuthorId (@PathVariable Integer authorid){
+        DefaultResponse df = new DefaultResponse();
+        Optional<Author> optionalAuthor = authorRepository.findByAuthorId(authorid);
+        if (optionalAuthor.isPresent()){
+            authorRepository.delete(optionalAuthor.get());
+            df.setStatus(Boolean.TRUE);
+            df.setMessage("Data Berhasil Dihapus");
+        } else {
+            df.setStatus(Boolean.FALSE);
+            df.setMessage("Data Tidak Ditemukan");
+        }
+        return df;
+
+        }
+    @PutMapping("/update/{id}")
+    public DefaultResponse updateById (@PathVariable Integer id, @RequestBody AuthorDto authorDto){
+        DefaultResponse defaultResponse = new DefaultResponse();
+        try{
+            Optional<Author> optionalAuthor = authorRepository.findByAuthorId(id);
+            Author author = optionalAuthor.get();
+            if (optionalAuthor.isPresent()){
+                author.setAuthorName(authorDto.getAuthorName());
+                author.setAuthorAddress(authorDto.getAuthorAddress());
+                author.setNoHp(author.getNoHp());
+                authorRepository.save(author);
+                defaultResponse.setStatus(Boolean.TRUE);
+                defaultResponse.setData(authorDto);
+                defaultResponse.setMessage("Succeeded update data");
+            }
+        }catch(Exception e){
+            defaultResponse.setStatus(Boolean.FALSE);
+            defaultResponse.setMessage("Failed to update data, Id was not found");
+        }
+        return defaultResponse;
+    }
+
+
+
+
+    private AuthorDto convertEntitytoDto(Author entity) {
+
+        AuthorDto dto = new AuthorDto();
+        dto.setAuthorId(entity.getAuthorId());
+        dto.setAuthorName(entity.getAuthorName());
+        dto.setAuthorAddress(entity.getAuthorAddress());
+        dto.setNoHp(entity.getNoHp());
+
+        return dto;
+
     }
 
     private Author convertDtoToEntity(AuthorDto authorDto) {
--- src/main/java/com/example/perpustakaan/repository/AuthorRepository.java
+++ src/main/java/com/example/perpustakaan/repository/AuthorRepository.java
@@ -10,4 +10,5 @@ public interface AuthorRepository extends JpaRepository<Author, Integer> {
 Optional<Author> findByAuthorId (Integer authorId);
 
 
+
 }
