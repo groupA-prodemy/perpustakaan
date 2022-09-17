9a3ead7b426e6560d26478f698943482b42308fa
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-17 13:15:42 +0700

    Change Origin in Peroustakaan application

--- src/main/java/com/example/perpustakaan/PerpustakaanApplication.java
+++ src/main/java/com/example/perpustakaan/PerpustakaanApplication.java
@@ -2,6 +2,10 @@ package com.example.perpustakaan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class PerpustakaanApplication {
@@ -10,4 +14,13 @@ public class PerpustakaanApplication {
 		SpringApplication.run(PerpustakaanApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
