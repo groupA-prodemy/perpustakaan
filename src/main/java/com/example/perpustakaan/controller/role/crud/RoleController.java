a3f08f3b4620fa91c7681ff8a85adc208067496f
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-19 12:47:27 +0700

    role cross origin

--- src/main/java/com/example/perpustakaan/controller/role/crud/RoleController.java
+++ src/main/java/com/example/perpustakaan/controller/role/crud/RoleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins="http://localhost:8080")
 @RestController
 @RequestMapping("/role")
 public class RoleController {
