ea9b9a94953d4d3ef75a4dfa0394c7d0dc2dd662
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-19 09:35:21 +0700

    Revision Role

--- src/main/java/com/example/perpustakaan/controller/role/converter/EntityToDto.java
+++ src/main/java/com/example/perpustakaan/controller/role/converter/EntityToDto.java
@@ -1,16 +1,16 @@
 package com.example.perpustakaan.controller.role.converter;
 
-import com.example.perpustakaan.model.dto.RoleDto;
+import com.example.perpustakaan.model.dto.RoleDtoList;
 import com.example.perpustakaan.model.entity.Role;
 
 
 public class EntityToDto {
 
-    public RoleDto convertEntityToDto(Role role){
-        RoleDto roleDto = new RoleDto();
-        roleDto.setRoleId(role.getRoleId());
-        roleDto.setRoleName(role.getRoleName());
-        return roleDto;
+    public RoleDtoList convertEntityToDto(Role role){
+        RoleDtoList roleDtoList = new RoleDtoList();
+        roleDtoList.setRoleId(role.getRoleId());
+        roleDtoList.setRoleName(role.getRoleName());
+        return roleDtoList;
     }
 
 }
--- src/main/java/com/example/perpustakaan/controller/role/crud/RoleController.java
+++ src/main/java/com/example/perpustakaan/controller/role/crud/RoleController.java
@@ -3,7 +3,8 @@ package com.example.perpustakaan.controller.role.crud;
 import com.example.perpustakaan.controller.role.converter.DtoToEntity;
 import com.example.perpustakaan.controller.role.converter.EntityToDto;
 import com.example.perpustakaan.model.dto.DefaultResponse;
-import com.example.perpustakaan.model.dto.RoleDto;
+import com.example.perpustakaan.model.dto.RoleDtoList;
+import com.example.perpustakaan.model.dto.RoleDtoSave;
 import com.example.perpustakaan.model.entity.Role;
 import com.example.perpustakaan.repository.RoleRepository;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +27,8 @@ public class RoleController {
     }
 
     @GetMapping("/list-role")
-    public List<RoleDto> getListRole() {
-        List<RoleDto> list = new ArrayList<>();
+    public List<RoleDtoList> getListRole() {
+        List<RoleDtoList> list = new ArrayList<>();
         for (Role i : roleRepository.findAll()) {
             list.add(entityToDto.convertEntityToDto(i));
         }
@@ -35,17 +36,19 @@ public class RoleController {
     }
 
     @PostMapping("/save-role")
-    public DefaultResponse<RoleDto> saveRole(@RequestBody RoleDto roleDto) {
-        Role role = dtoToEntity.convertDtoToEntity(roleDto);
-        DefaultResponse<RoleDto> defaultResponse = new DefaultResponse<>();
-        Optional<Role> optionalRole = roleRepository.findById(roleDto.getRoleId());
+    public DefaultResponse<RoleDtoList> saveRole(@RequestBody RoleDtoSave roleDtoSave, RoleDtoList roleDtoList) {
+        Role role = dtoToEntity.convertDtoToEntity(roleDtoSave);
+        DefaultResponse<RoleDtoList> defaultResponse = new DefaultResponse<>();
+        Optional<Role> optionalRole = roleRepository.findByRoleName(roleDtoSave.getRoleName());
         if (optionalRole.isPresent()) {
             defaultResponse.setStatus(Boolean.FALSE);
             defaultResponse.setMessage("Failed to save data, data was exists");
         } else {
             roleRepository.save(role);
+            roleDtoList.setRoleId(roleRepository.findByRoleName(roleDtoSave.getRoleName()).get().getRoleId());
+            roleDtoList.setRoleName(roleRepository.findByRoleName(roleDtoSave.getRoleName()).get().getRoleName());
             defaultResponse.setStatus(Boolean.TRUE);
-            defaultResponse.setData(roleDto);
+            defaultResponse.setData(roleDtoList);
             defaultResponse.setMessage("Succeeded to save data");
         }
         return defaultResponse;
@@ -55,29 +58,36 @@ public class RoleController {
     public DefaultResponse deleteById(@PathVariable("id") Integer id) {
         DefaultResponse defaultResponse = new DefaultResponse();
         Optional<Role> optionalRole = roleRepository.findById(id);
-        if (optionalRole.isPresent()) {
-            roleRepository.delete(optionalRole.get());
-            defaultResponse.setStatus(Boolean.TRUE);
-            defaultResponse.setMessage("Succeeded delete data");
-        } else {
+        try {
+            if (optionalRole.isPresent()) {
+                roleRepository.delete(optionalRole.get());
+                defaultResponse.setStatus(Boolean.TRUE);
+                defaultResponse.setMessage("Succeeded delete data");
+            } else {
+                defaultResponse.setStatus(Boolean.FALSE);
+                defaultResponse.setMessage("Failed to delete data, data was not found");
+            }
+
+        }catch (Exception e){
             defaultResponse.setStatus(Boolean.FALSE);
-            defaultResponse.setMessage("Failed to delete data, data was not found");
+            defaultResponse.setMessage("Failed to delete data. Your data was referenced from table t_user. Change that before you want to delete this data");
         }
         return defaultResponse;
     }
 
     @PutMapping("/update/{id}")
-    public DefaultResponse updateById(@PathVariable Integer id, @RequestBody RoleDto roleDto) {
+    public DefaultResponse updateById(@PathVariable Integer id, @RequestBody RoleDtoSave roleDtoSave, RoleDtoList roleDtoList) {
         DefaultResponse defaultResponse = new DefaultResponse();
         try {
             Optional<Role> optionalRole = roleRepository.findById(id);
             Role role = optionalRole.get();
             if (optionalRole.isPresent()) {
-                role.setRoleId(roleDto.getRoleId());
-                role.setRoleName(roleDto.getRoleName());
+                role.setRoleName(roleDtoSave.getRoleName());
                 roleRepository.save(role);
+                roleDtoList.setRoleId(roleRepository.findById(id).get().getRoleId());
+                roleDtoList.setRoleName(roleRepository.findById(id).get().getRoleName());
                 defaultResponse.setStatus(Boolean.TRUE);
-                defaultResponse.setData(roleDto);
+                defaultResponse.setData(roleDtoList);
                 defaultResponse.setMessage("Succeeded update data");
             }
         } catch (Exception e) {
