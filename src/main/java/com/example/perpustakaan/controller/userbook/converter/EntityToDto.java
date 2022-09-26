b8c8d2ff79cada2a44d8f1319af22b0c225be25b
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-26 07:37:34 +0700

    --

--- src/main/java/com/example/perpustakaan/controller/userbook/converter/EntityToDto.java
+++ src/main/java/com/example/perpustakaan/controller/userbook/converter/EntityToDto.java
@@ -1,21 +1,30 @@
 package com.example.perpustakaan.controller.userbook.converter;
 
+import com.example.perpustakaan.model.dto.DefaultResponse;
 import com.example.perpustakaan.model.dto.userbook.UserBookDto;
 import com.example.perpustakaan.model.entity.UserBook;
 
 public class EntityToDto {
 
-    public UserBookDto convertEntityToDto (UserBook userBook) {
+    public DefaultResponse<UserBookDto> convertEntityToDto (UserBook userBook) {
         UserBookDto userBookDto = new UserBookDto();
-        userBookDto.setUserbookId(userBook.getUserBookId());
-        userBookDto.setBookId(userBook.getBook().getBookId());
-        userBookDto.setBookTitle(userBook.getBook().getBookTitle());
-        userBookDto.setUserId(userBook.getUser().getId());
-        userBookDto.setUserName(userBook.getUser().getUsername());
-        userBookDto.setStartDate(userBook.getStartDate());
-        userBookDto.setDueDate(userBook.getDueDate());
-        userBookDto.setReturnDate(userBook.getReturnDate());
+        DefaultResponse df = new DefaultResponse();
+        try{
+            userBookDto.setUserbookId(userBook.getUserBookId());
+            userBookDto.setBookId(userBook.getBook().getBookId());
+            userBookDto.setBookTitle(userBook.getBook().getBookTitle());
+            userBookDto.setUserId(userBook.getUser().getId());
+            userBookDto.setUserName(userBook.getUser().getUsername());
+            userBookDto.setStartDate(userBook.getStartDate());
+            userBookDto.setDueDate(userBook.getDueDate());
+            userBookDto.setReturnDate(userBook.getReturnDate());
+            df.setStatus(Boolean.TRUE);
+            df.setMessage("Ini berhasil");
+        }catch (Exception e){
+            df.setStatus(Boolean.FALSE);
+            df.setMessage("Ini error karena ID ga ada");
 
-        return userBookDto;
+        }
+        return df;
     }
 }
